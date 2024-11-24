c99afe6832fc1483badc6729b95352872527f670
Bomin Kwon <101239440+pingowl@users.noreply.github.com>
2024-11-24 10:05:55 +0900

    Feat: 푸시알림 기록 조회 (#45)

--- src/main/java/drugdrop/BE/repository/NotificationRepository.java
+++ src/main/java/drugdrop/BE/repository/NotificationRepository.java
@@ -3,5 +3,8 @@ package drugdrop.BE.repository;
 import drugdrop.BE.domain.Notification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
+    List<Notification> findAllByMemberId(Long memberId);
 }
